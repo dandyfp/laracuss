faff82bda8770924ee38657799342817449211de
dandy <dandyfirmansyah1327@gmail.com>
2024-07-12 09:37:04 +0700

    section statistic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,8 +49,37 @@
               </div>
             </div>
           </nav>
-          <section>
-            
+          <section class="container hero">
+            <div class="row align-items-center h-100">
+              <div class="col-12 col-lg-6">
+                <h1>The Laravel <br/>Community Forum</h1>
+                <p class="mb-4">Empowering the Laravel Community to connect, share and learn</p>
+                <a href="#" class="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+                <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Disscusions</a>
+              </div>
+              <div class="col-12 col-lg-6 h-315px order-first order-lg-last mb-3 mb-lg-0">
+                <img class="hero-image float-lg-end" src="{{url("assets/images/image-hero.png")}}" alt="">
+              </div>
+            </div>
+          </section>
+          <section class="container min-h-372px">
+            <div class="row">
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{url("assets/images/discussions.png")}}" alt="Discussions">
+                <h2>Discussion</h2>
+                <p class="fs-3">3333</p>
+              </div>
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{url("assets/images/answers.png")}}" alt="Answers">
+                <h2>Answers</h2>
+                <p class="fs-3">3333</p>
+              </div>
+              <div class="col-12 col-lg-4 text-center">
+                <img class="promote-icon mb-2" src="{{url("assets/images/users.png")}}" alt="Users">
+                <h2>Users</h2>
+                <p class="fs-3">3333</p>
+              </div>
+            </div>
           </section>
           <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
