6d2eb6b9341dd020f89e6b5d7fca0e83631a52ef
dandyfp <dandyfirmansyah13@gmail.com>
2024-09-03 23:29:00 +0700

    Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,6 +81,97 @@
               </div>
             </div>
           </section>
+          <section class="bg-gray">
+            <div class="container py-80px">
+              <h2 class="text-center mb-5">Help Others</h2>
+              <div class="row">
+                <div class="col-12 col-lg-4 mb-3">
+                  <div class="card">
+                    <a href="#">
+                      <h3>How to add a custom validation in laravel?</h3>
+                    </a>
+                    <div>
+                      <p class="mb-5">
+                        I am working on a blogging aplication in laravel 8. There are 4 under roles, among which, thef
+                      </p>
+                      <div class="row">
+                        <div class="col" me-1 me-lg-2>
+                          <a href="#">
+                            <span class = "badge rounded-pill text-bg-light">
+                              Eloquent
+                            </span>
+                          </a>
+                        </div>
+                        <div class="col-5 col-lg-7">
+                          <div class="avatar-sm-wrapper d-inline-block">
+                            <a href="#" class = "me-1">
+                              <img src="{{url("assets/images/magnifiar.png")}}" class="avatar rounded-circle" alt="">
+                            </a>
+                          </div>
+                          <span class="fs-12px" >
+                            <a href="#" class="me-1 fw-bold">
+                              Dandy
+                            </a>
+                            <span class="color-gray">
+                              7 hours ago
+                            </span>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="container min-h-372px d-flex flex-column align-items-center justify-content-center">
+            <h2>Ready to contribute?</h2>
+            <p class = "mb-4">want to make a big impact?</p>
+            <div class="text-center">
+            <a href="#" class="btn btn-primary me-2 mb-2 mb-lg-0">Sign Up</a>
+            <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Disscusions</a>
+            </div>
+          </section>
+          <footer class="py-80px">
+            <div class="container">
+              <div class="row">
+                <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                  <img src="{{url("assets/images/logo_white.png")}}" alt="Laracuss logo" class="h-32px mb-4">
+                  <p class="text-white">Empowering laravel community</br>to connect. share and learn.</p>
+                </div>
+                <div<div class="col-12 col-lg-6 me-auto">
+                  <div class="d-flex flex-column flex-lg-row justify-content-end">
+                    <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                      <p class="fw-bold fs-5 text-white text-nowrap">
+                        contact us
+                      </p>
+                      <ul class="list-unstyled">
+                          <li class="text-white">
+                            <a href="mailto:hi@laracuss.com">hi@laracuss.com</a>
+                          </li>
+                      </ul>
+                    </div>
+                    <div class="d-flex flex-column">
+                    <p class="fw-bold fs-5 text-white">
+                        Links
+                      </p>
+                      <ul class="list-unstyled">
+                          <li class="text-white">
+                            <a href="#">Home</a>
+                          </li>
+                          <li class="text-white">
+                            <a href="#">Discussions</a>
+                          </li>
+                          <li class="text-white">
+                            <a href="#">About Us</a>
+                          </li>
+                      </ul>
+                    </div>
+                  </div> 
+                </div>
+              </div>
+            </div>
+          </footer>
           <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     </body>
 </html>
